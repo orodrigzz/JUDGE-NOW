0eaa9aa54c7e7f76e6af9ba3b2bafff4b3c0c757
Carlos Sánchez Abad <98030188+thecharlie00@users.noreply.github.com>
2023-02-10 12:53:16 +0100

    Sistema de pickear objetos en proceso

--- Assets/Scripts/Carlos/PickUpSystem.cs
+++ Assets/Scripts/Carlos/PickUpSystem.cs
@@ -6,28 +6,47 @@ public class PickUpSystem : MonoBehaviour
 {
     public Vector3[] rayOrigins;
     public LayerMask layer;
+    public GameObject parent;
+    public Rigidbody itemPicked;
 
     private void Update()
     {
-        foreach(Vector3 origin in rayOrigins)
-        {
-            Ray ray = new Ray(transform.position + origin, transform.forward);
-            RaycastHit hit;
+            foreach (Vector3 origin in rayOrigins)
+            {
+                Ray ray = new Ray(transform.position + origin, transform.forward);
+                RaycastHit hit;
             if(Physics.Raycast(ray, out hit,10f, layer))
             {
-                if (Input.GetMouseButton(0))
-                {                                  
-                    hit.transform.position = transform.position;
-                    hit.transform.rotation = transform.rotation;
-                    Rigidbody rigidbody = hit.transform.GetComponent<Rigidbody>();
-                    if (rigidbody != null)
+                if (Input.GetMouseButtonDown(0))
+                {
+                    itemPicked = hit.rigidbody;
+                    itemPicked.transform.SetParent(parent.transform);
+                    itemPicked.transform.localScale = Vector3.one;
+
+
+                    
+                    if (itemPicked != null)
                     {
-                        rigidbody.useGravity = false;
+                        itemPicked.useGravity = false;
                     }
                 }
             }
+            else
+            {
+                if (Input.GetMouseButtonDown(1))
+                {
+                    Debug.Log("Adoptado");
+                    if(itemPicked != null)
+                    {
+                        itemPicked.transform.SetParent(null);
+                        itemPicked.useGravity = true;
+                    }
+                    itemPicked = null;
+                }
+            }
            
-            Debug.DrawRay(transform.position + origin, transform.forward * 100, Color.green);
-        }
+                Debug.DrawRay(transform.position + origin, transform.forward * 100, Color.green);
+            }
+        
     }
 }
