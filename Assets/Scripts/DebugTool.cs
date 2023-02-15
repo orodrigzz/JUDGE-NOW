6006f96afb75f8669c2ade457225a997b3c8db81
Carlos Sánchez Abad <98030188+thecharlie00@users.noreply.github.com>
2023-02-15 19:25:14 +0100

    skibidipoop

---
+++ Assets/Scripts/DebugTool.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugTool : MonoBehaviour
+{
+
+    void OnDrawGizmos()
+    {
+        // Draw a yellow sphere at the transform's position
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawSphere(transform.position, 1);
+    }
+}
