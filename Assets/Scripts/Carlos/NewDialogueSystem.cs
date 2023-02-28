77f0c9c6f7efd0a8408f36d76778ad59ff40ffdf
Carlos Sánchez Abad <98030188+thecharlie00@users.noreply.github.com>
2023-02-28 13:54:16 +0100

    Updated Dialogue System

---
+++ Assets/Scripts/Carlos/NewDialogueSystem.cs
@@ -0,0 +1,126 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewDialogueSystem : MonoBehaviour
+{
+    [System.Serializable]
+   public struct Dialogues
+   {
+        public Text[] sentences;
+        public Text nameText;
+        public GameObject textBox;
+
+        public string[] dialogueLines;
+        public string characterName;
+
+        public int sentenceIndex;
+        public int diactivateCount;
+
+        public bool currentDialogueEnded;
+   }
+    [NonReorderable]
+
+    public Dialogues[] dialogues;
+
+    public bool dialogueOn;
+    public int dialoguesIndex = -1;
+   
+
+    private void Awake()
+    {
+        for(int i = 0; i < dialogues.Length; i++)
+        {
+            dialogues[i].textBox.SetActive(false);
+            dialogues[i].currentDialogueEnded = false;
+        }
+        dialogueOn = false;
+    }
+    void Start()
+    {
+        
+    }
+
+ 
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            //Funcion que da inicio al primer dialogo
+            //Ira vinculada al collaider
+            CurrentDialogue();
+        }
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            //Funcion que ira vinculada al input una vez iniciado el dialogo
+            if (dialogueOn)
+            {
+                OnDialogue();
+            }
+           
+        }
+        if(dialoguesIndex >= 0)
+        {
+            if (dialogueOn)
+            {
+               
+                //Comprobamos si a acabado el dialogo actual y si es asi pasamos al siguiente
+                if (dialogues[dialoguesIndex].sentenceIndex >= dialogues[dialoguesIndex].sentences.Length-1)
+                {
+                    dialogues[dialoguesIndex].currentDialogueEnded = true;
+                    dialoguesIndex++;
+                    dialogues[dialoguesIndex-1].textBox.SetActive(false);
+                }
+
+            }
+
+
+            //comprobamos si quedan mas dialogos 
+            if (dialoguesIndex > dialogues.Length-1)
+            {
+                for (int i = 0; i < dialogues.Length; i++)
+                {
+                    dialogues[i].textBox.SetActive(false);
+                    dialogues[i].currentDialogueEnded = false;
+                }
+                dialogueOn = false;
+            }
+
+        }
+        
+        
+    }
+    public void CurrentDialogue()
+    {
+        if(dialoguesIndex <= 0)
+        {
+            dialoguesIndex++;
+        }
+        dialogueOn = true;
+        OnDialogue();   
+    }
+    public void OnDialogue()
+    {
+            dialogues[dialoguesIndex].nameText.text = null;
+            dialogues[dialoguesIndex].nameText.text += dialogues[dialoguesIndex].characterName;
+
+            if (dialogues[dialoguesIndex].sentenceIndex < dialogues[dialoguesIndex].sentences.Length)
+            {
+                dialogues[dialoguesIndex].textBox.SetActive(true);
+                dialogues[dialoguesIndex].currentDialogueEnded = false;
+                dialogues[dialoguesIndex].sentenceIndex++;
+                dialogues[dialoguesIndex].sentences[dialogues[dialoguesIndex].sentenceIndex].enabled = true;
+                dialogues[dialoguesIndex].sentences[dialogues[dialoguesIndex].sentenceIndex].text += dialogues[dialoguesIndex].dialogueLines[dialogues[dialoguesIndex].sentenceIndex];
+            }
+            if (dialogues[dialoguesIndex].sentenceIndex > 0)
+            {
+                dialogues[dialoguesIndex].diactivateCount = dialogues[dialoguesIndex].sentenceIndex - 1;
+                dialogues[dialoguesIndex].sentences[dialogues[dialoguesIndex].diactivateCount].enabled = false;
+           }
+        
+            
+               
+    }
+}
