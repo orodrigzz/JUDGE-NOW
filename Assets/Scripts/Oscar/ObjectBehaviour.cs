af60689dc8ba9145c9fab85579ba1ca91ac64613
orodrigzz <oscarrod18503@gmail.com>
2023-04-20 12:33:58 +0200

    Ajustes props

--- Assets/Scripts/Oscar/ObjectBehaviour.cs
+++ Assets/Scripts/Oscar/ObjectBehaviour.cs
@@ -4,11 +4,38 @@ using UnityEngine;
 
 public class ObjectBehaviour : MonoBehaviour
 {
+    [SerializeField] AudioSource Bonk;
+    [SerializeField] AudioSource Interruptor;
+
+    [SerializeField] AudioSource Ouch;
+    [SerializeField] AudioSource OuchWoman;
+
     private void OnCollisionEnter(Collision collision)
     {
+        if (collision.gameObject.tag == "NPC" || collision.gameObject.tag == "Testigo_M")
+        {
+            Bonk.Play();
+            Ouch.Play();
+            StartCoroutine(WaitForDestroy(1.5f));
+        }
+
+        if (collision.gameObject.tag == "NPC_W" || collision.gameObject.tag == "Testigo_W")
+        {
+            Bonk.Play();
+            OuchWoman.Play();
+            StartCoroutine(WaitForDestroy(1.5f));
+        }
+
         if (collision.gameObject.tag == "Ground" || collision.gameObject.tag == "Door" || collision.gameObject.tag == "Atril")
         {
-            StartCoroutine(WaitForDestroy(1));
+            StartCoroutine(WaitForDestroy(1.5f));
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Switch")
+        {
+            Interruptor.Play();
         }
     }
 
