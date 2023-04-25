dd97bed2eaf870aa379917490f0cb57bea7925d9
orodrigzz <oscarrod18503@gmail.com>
2023-04-25 10:58:05 +0200

    Escenario2

--- Assets/Scripts/Carlos/NextCase.cs
+++ Assets/Scripts/Carlos/NextCase.cs
@@ -36,6 +36,42 @@ public class NextCase : MonoBehaviour
         }
         if (GAME_MANAGER._GAME_MANAGER.lastScene == "Case3")
         {
+            SceneManager.LoadScene("Scenario2");
+            GAME_MANAGER._GAME_MANAGER.isGamePaused = false;
+            GAME_MANAGER._GAME_MANAGER.lastScene = null;
+            GAME_MANAGER._GAME_MANAGER.endDialogue = false;
+            GAME_MANAGER._GAME_MANAGER.isInspecting = false;
+            GAME_MANAGER._GAME_MANAGER.isPicked = false;
+        }
+        if (GAME_MANAGER._GAME_MANAGER.lastScene == "Scenario2")
+        {
+            SceneManager.LoadScene("Case4");
+            GAME_MANAGER._GAME_MANAGER.isGamePaused = false;
+            GAME_MANAGER._GAME_MANAGER.lastScene = null;
+            GAME_MANAGER._GAME_MANAGER.endDialogue = false;
+            GAME_MANAGER._GAME_MANAGER.isInspecting = false;
+            GAME_MANAGER._GAME_MANAGER.isPicked = false;
+        }
+        if (GAME_MANAGER._GAME_MANAGER.lastScene == "Case4")
+        {
+            SceneManager.LoadScene("Case5");
+            GAME_MANAGER._GAME_MANAGER.isGamePaused = false;
+            GAME_MANAGER._GAME_MANAGER.lastScene = null;
+            GAME_MANAGER._GAME_MANAGER.endDialogue = false;
+            GAME_MANAGER._GAME_MANAGER.isInspecting = false;
+            GAME_MANAGER._GAME_MANAGER.isPicked = false;
+        }
+        if (GAME_MANAGER._GAME_MANAGER.lastScene == "Case5")
+        {
+            SceneManager.LoadScene("Case6");
+            GAME_MANAGER._GAME_MANAGER.isGamePaused = false;
+            GAME_MANAGER._GAME_MANAGER.lastScene = null;
+            GAME_MANAGER._GAME_MANAGER.endDialogue = false;
+            GAME_MANAGER._GAME_MANAGER.isInspecting = false;
+            GAME_MANAGER._GAME_MANAGER.isPicked = false;
+        }
+        if (GAME_MANAGER._GAME_MANAGER.lastScene == "Case6")
+        {
             SceneManager.LoadScene("Win");
             GAME_MANAGER._GAME_MANAGER.isGamePaused = false;
             GAME_MANAGER._GAME_MANAGER.lastScene = null;
